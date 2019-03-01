bf395292144efce935062e00200a89945d939176
victor110298 <44604893+victor110298@users.noreply.github.com>
2019-03-02 01:05:20 +0200

    Add class CarDoor

---
+++ src/sto/CarDoor.java
@@ -0,0 +1,67 @@
+package sto;
+
+public class CarDoor{
+    private boolean closeDoor;
+    private boolean closeWindow;
+
+
+    public CarDoor() {
+        closeDoor=true;
+        closeWindow=true;
+
+    }
+
+    public CarDoor(boolean openDoor, boolean openWindow) {
+        this.closeDoor = openDoor;
+        this.closeWindow = openWindow;
+    }
+
+    public void openDoor(){
+        if (!closeDoor){
+            setDoorIsClosed(false);
+        }
+    }
+    public void closeDoor(){
+        if(!closeDoor){
+            setDoorIsClosed(true);
+        }
+    }
+    public void switchDoor(){
+        setDoorIsClosed(!closeDoor);
+    }
+    public void openWindow() {
+        if (!closeWindow) {
+            setWindowIsClosed(false);
+        }
+
+    }
+    public void closeWindow(){
+        if(!closeWindow){
+            setWindowIsClosed(true);
+        }
+    }
+    public void switchWindow(){
+        setWindowIsClosed(!closeWindow);
+    }
+
+    public boolean isDoorClosed() {
+        return closeDoor;
+    }
+
+    public void setDoorIsClosed(boolean closeDoor) {
+        this.closeDoor = closeDoor;
+    }
+
+    public boolean isWindowClosed() {
+        return closeWindow;
+    }
+
+    public void setWindowIsClosed(boolean windowIsOpened) {
+        this.closeDoor = windowIsOpened;
+    }
+    public void printCarDoorState() {
+        System.out.println("The door now is " + (isDoorClosed() ? "opened" : "closed")
+                + " and window now is " + (isWindowClosed() ? "opened" : "closed"));
+    }
+
+}
